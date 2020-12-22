b9c4c57ca4bc6c4e7d37547fd2fc63c717b33c5b
VanTrung <trung.pv194@gmail.com>
2020-12-22 20:32:18 +0700

    update sql file

--- bigproject.sql
+++ bigproject.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `bigproject` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `bigproject`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bigproject
@@ -30,7 +28,7 @@ CREATE TABLE `airport` (
   `address` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -51,22 +49,24 @@ DROP TABLE IF EXISTS `airticket`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `airticket` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `type` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` float NOT NULL,
   `date` date NOT NULL,
+  `seat_id` int NOT NULL,
   `employee_id` int NOT NULL,
   `customer_id` int NOT NULL,
   `flight_id` int NOT NULL,
   PRIMARY KEY (`id`),
+  KEY `seat_id` (`seat_id`),
   KEY `employee_id` (`employee_id`),
   KEY `customer_id` (`customer_id`),
   KEY `flight_id` (`flight_id`),
-  CONSTRAINT `airticket_ibfk_1` FOREIGN KEY (`id`) REFERENCES `seat` (`id`),
+  CONSTRAINT `airticket_ibfk_1` FOREIGN KEY (`seat_id`) REFERENCES `seat` (`id`),
   CONSTRAINT `airticket_ibfk_2` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
   CONSTRAINT `airticket_ibfk_3` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
   CONSTRAINT `airticket_ibfk_4` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,7 +75,41 @@ CREATE TABLE `airticket` (
 
 LOCK TABLES `airticket` WRITE;
 /*!40000 ALTER TABLE `airticket` DISABLE KEYS */;
+INSERT INTO `airticket` VALUES (1,'Phổ thông',300000,'2020-12-22',1,1,1,1),(2,'Phổ thông',250000,'2020-12-22',5,1,2,2),(3,'Phổ Thông',300000,'2020-11-22',2,1,1,1),(4,'Phổ Thông',800000,'2020-10-20',4,1,1,1);
 /*!40000 ALTER TABLE `airticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book_ticket`
+--
+
+DROP TABLE IF EXISTS `book_ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book_ticket` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` float NOT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `customer_id` int NOT NULL,
+  `flight_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `flight_id` (`flight_id`),
+  CONSTRAINT `book_ticket_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  CONSTRAINT `book_ticket_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`id`),
+  CONSTRAINT `book_ticket_chk_1` CHECK ((`status` in (0,1)))
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_ticket`
+--
+
+LOCK TABLES `book_ticket` WRITE;
+/*!40000 ALTER TABLE `book_ticket` DISABLE KEYS */;
+INSERT INTO `book_ticket` VALUES (1,'Phổ thông',300000,1,1,1),(2,'Phổ thông',250000,1,2,2);
+/*!40000 ALTER TABLE `book_ticket` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -92,7 +126,7 @@ CREATE TABLE `customer` (
   `identity_card` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `address` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,6 +135,7 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'Nguyễn Văn A','0905340601','245408601','TP HCM'),(2,'Nguyễn Văn B','0905340602','0905340602','TP HCM');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +156,7 @@ CREATE TABLE `employee` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,6 +165,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'Phan Văn Trung','Nhân Viên','0905340611','477/40 Nguyễn Văn Công',1);
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,6 +181,7 @@ CREATE TABLE `flight` (
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `departure_day` date NOT NULL,
   `flight_time` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` float NOT NULL,
   `plane_id` int NOT NULL,
   `departure_airport_id` int NOT NULL,
   `arrival_airport_id` int NOT NULL,
@@ -158,7 +195,7 @@ CREATE TABLE `flight` (
   CONSTRAINT `flight_ibfk_2` FOREIGN KEY (`departure_airport_id`) REFERENCES `airport` (`id`),
   CONSTRAINT `flight_ibfk_3` FOREIGN KEY (`arrival_airport_id`) REFERENCES `airport` (`id`),
   CONSTRAINT `flight_ibfk_4` FOREIGN KEY (`intermediate_airport_id`) REFERENCES `intermediate_airport` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -167,7 +204,7 @@ CREATE TABLE `flight` (
 
 LOCK TABLES `flight` WRITE;
 /*!40000 ALTER TABLE `flight` DISABLE KEYS */;
-INSERT INTO `flight` VALUES (1,'HCM đi Hà Nội','2020-12-15','7:00 AM',1,1,2,2),(2,'HCM đi Hà Nội','2020-12-15','9:00 AM',2,1,2,2),(3,'Đăk Lăk đi Hà Nội','2020-12-15','7:00 AM',2,3,2,2);
+INSERT INTO `flight` VALUES (1,'HCM đi Hà Nội','2020-12-22','7:00 AM',300000,1,1,2,1),(2,'Đăk Lăk đi Hà Nội','2020-12-22','7:00 AM',250000,2,3,2,1);
 /*!40000 ALTER TABLE `flight` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -184,7 +221,7 @@ CREATE TABLE `intermediate_airport` (
   `address` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -193,7 +230,7 @@ CREATE TABLE `intermediate_airport` (
 
 LOCK TABLES `intermediate_airport` WRITE;
 /*!40000 ALTER TABLE `intermediate_airport` DISABLE KEYS */;
-INSERT INTO `intermediate_airport` VALUES (1,'Sân bay Đà Lạt','Lâm Đồng','Đang hoạt động'),(2,'Sân bay Đà Nẵng','Đà Nẵng','Đà Nẵng');
+INSERT INTO `intermediate_airport` VALUES (1,'Sân bay Đà Nẵng','Đà Nẵng','Đang hoạt động');
 /*!40000 ALTER TABLE `intermediate_airport` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -221,34 +258,8 @@ CREATE TABLE `plane` (
 
 LOCK TABLES `plane` WRITE;
 /*!40000 ALTER TABLE `plane` DISABLE KEYS */;
-INSERT INTO `plane` VALUES (1,'Boeing 777-200LR','Boeing',200,100,300),(2,'Boeing 777-200','Boeing',150,150,300);
+INSERT INTO `plane` VALUES (1,'Boeing 777-200','Boeing',150,150,300),(2,'Boeing 777-200LR','Boeing',150,150,300);
 /*!40000 ALTER TABLE `plane` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `plane_seat`
---
-
-DROP TABLE IF EXISTS `plane_seat`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `plane_seat` (
-  `plane_id` int NOT NULL,
-  `seat_id` int NOT NULL,
-  PRIMARY KEY (`plane_id`,`seat_id`),
-  KEY `seat_id` (`seat_id`),
-  CONSTRAINT `plane_seat_ibfk_1` FOREIGN KEY (`plane_id`) REFERENCES `plane` (`id`),
-  CONSTRAINT `plane_seat_ibfk_2` FOREIGN KEY (`seat_id`) REFERENCES `seat` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `plane_seat`
---
-
-LOCK TABLES `plane_seat` WRITE;
-/*!40000 ALTER TABLE `plane_seat` DISABLE KEYS */;
-/*!40000 ALTER TABLE `plane_seat` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -262,9 +273,12 @@ CREATE TABLE `seat` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` tinyint(1) NOT NULL,
+  `plane_id` int NOT NULL,
   PRIMARY KEY (`id`),
+  KEY `plane_id` (`plane_id`),
+  CONSTRAINT `seat_ibfk_1` FOREIGN KEY (`plane_id`) REFERENCES `plane` (`id`),
   CONSTRAINT `seat_chk_1` CHECK ((`status` in (0,1)))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -273,6 +287,7 @@ CREATE TABLE `seat` (
 
 LOCK TABLES `seat` WRITE;
 /*!40000 ALTER TABLE `seat` DISABLE KEYS */;
+INSERT INTO `seat` VALUES (1,'A1',1,1),(2,'A2',0,1),(3,'A3',0,1),(4,'A4',0,1),(5,'A1',1,2),(6,'A2',0,2),(7,'A3',0,2),(8,'A4',0,2);
 /*!40000 ALTER TABLE `seat` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -295,7 +310,7 @@ CREATE TABLE `user` (
   `user_role` enum('CUSTOMER','EMPLOYEE','ADMIN') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `user_chk_1` CHECK ((`active` in (0,1)))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -304,6 +319,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Phan Văn Trung','trung','202cb962ac59075b964b07152d234b70','1851050165trung@ou.edu.vn',NULL,1,'2020-12-22','EMPLOYEE');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -316,4 +332,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-15 18:02:26
+-- Dump completed on 2020-12-22 20:32:05
